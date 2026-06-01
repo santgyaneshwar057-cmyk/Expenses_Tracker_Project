aa3a6345b71a84ebca8ba56db2980edd8b4f3bc7
santgyaneshwar057-cmyk <santgyaneshwar057@gmail.com>
2026-06-01 13:10:15 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// This file exports your Firebase Configuration.
+export const firebaseConfig = {
+    apiKey: "AIzaSyABxewcGwie_nwEnOHDdX5Cf5PYWGGI2po",
+    authDomain: "expense-tracker-71738.firebaseapp.com",
+    projectId: "expense-tracker-71738",
+    storageBucket: "expense-tracker-71738.firebasestorage.app",
+    messagingSenderId: "920781327773",
+    appId: "1:920781327773:web:8c1857541488d8743a3374",
+    measurementId: "G-LTQQYZVEHR"
+};
